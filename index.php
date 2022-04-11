9c7a9df80f469ab03b901a0c4a2fb440559c0d4a
MGPasinetti <mg.pasinetti@gmail.com>
2022-04-11 17:51:28 +0200

    QandA printed and fixed with css

--- index.php
+++ index.php
@@ -49,7 +49,11 @@ Completare il resto della pagina. -->
                 </li>
                 <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
             </ol>
-            <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>",
+            <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
+            <h2>Stabilire il paese associato al tuo account</h2>
+            <p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</p>
+            <p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p>
+            <p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.</p>",
         ],
         [
             'Q' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
